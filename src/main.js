66f6e13b0b2e806be7c799197fb4829839d54749
junw <wo1261931780@gmail.com>
2024-10-18 13:08:07 +0800

    用vue3重写

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
+/*
+ * @Author: junw wo1261931780@gmail.com
+ * @Date: 2024-10-18 08:44:03
+ * @LastEditors: junw wo1261931780@gmail.com
+ * @LastEditTime: 2024-10-18 09:26:25
+ * @FilePath: \chooseCollegeVue\src\main.js
+ * @Description: 1111
+ *
+ * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved.
+ */
 import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +21,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(ElementPlus)
 app.mount('#app')
--- vitest.config.js
+++ vitest.config.js
@@ -1,6 +1,20 @@
+/*
+ * @Author: junw wo1261931780@gmail.com
+ * @Date: 2024-10-18 08:44:03
+ * @LastEditors: junw wo1261931780@gmail.com
+ * @LastEditTime: 2024-10-18 11:53:58
+ * @FilePath: \chooseCollegeVue\vitest.config.js
+ * @Description: 1111
+ *
+ * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved.
+ */
 import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import ElementPlus from 'unplugin-element-plus/vite'
 
 export default mergeConfig(
   viteConfig,
@@ -9,6 +23,15 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url))
-    }
+    }, plugins: [
+      // ...
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+      ElementPlus(),
+    ],
   })
 )
