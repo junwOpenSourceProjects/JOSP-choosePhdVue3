324790e3dd1a5a7b3aa2b1a15e824ae684e919f4
junw <wo1261931780@gmail.com>
2024-10-18 13:52:13 +0800

    Update DemoContainer.vue

--- src/views/DemoContainer.vue
+++ src/views/DemoContainer.vue
@@ -1,83 +1,54 @@
+<!-- src/views/DemoContainer.vue -->
 <template>
-  <el-container class="layout-container-demo" style="height: 500px">
-    <el-aside width="200px">
-      <el-scrollbar>
-        <el-menu :default-openeds="['1', '3']">
-          <el-sub-menu index="1">
-            <template #title>
-              <el-icon><message /></el-icon>Navigator One
-            </template>
-            <el-menu-item-group>
-              <template #title>Group 1</template>
-              <el-menu-item index="1-1">Option 1</el-menu-item>
-              <el-menu-item index="1-2">Option 2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="Group 2">
-              <el-menu-item index="1-3">Option 3</el-menu-item>
-            </el-menu-item-group>
-            <el-sub-menu index="1-4">
-              <template #title>Option4</template>
-              <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
-            </el-sub-menu>
-          </el-sub-menu>
-          <el-sub-menu index="2">
-            <template #title>
-              <el-icon><icon-menu /></el-icon>Navigator Two
-            </template>
-            <el-menu-item-group>
-              <template #title>Group 1</template>
-              <el-menu-item index="2-1">Option 1</el-menu-item>
-              <el-menu-item index="2-2">Option 2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="Group 2">
-              <el-menu-item index="2-3">Option 3</el-menu-item>
-            </el-menu-item-group>
-            <el-sub-menu index="2-4">
-              <template #title>Option 4</template>
-              <el-menu-item index="2-4-1">Option 4-1</el-menu-item>
-            </el-sub-menu>
-          </el-sub-menu>
-          <el-sub-menu index="3">
-            <template #title>
-              <el-icon><setting /></el-icon>Navigator Three
-            </template>
-            <el-menu-item-group>
-              <template #title>Group 1</template>
-              <el-menu-item index="3-1">Option 1</el-menu-item>
-              <el-menu-item index="3-2">Option 2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="Group 2">
-              <el-menu-item index="3-3">Option 3</el-menu-item>
-            </el-menu-item-group>
-            <el-sub-menu index="3-4">
-              <template #title>Option 4</template>
-              <el-menu-item index="3-4-1">Option 4-1</el-menu-item>
-            </el-sub-menu>
-          </el-sub-menu>
-        </el-menu>
-      </el-scrollbar>
-    </el-aside>
+  <el-container class="layout-container-demo">
+    <!-- 顶部 Header -->
+    <el-header class="header">
+      <div class="toolbar">
+        <el-dropdown>
+          <el-icon class="dropdown-icon">
+            <Setting />
+          </el-icon>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item>View</el-dropdown-item>
+              <el-dropdown-item>Add</el-dropdown-item>
+              <el-dropdown-item>Delete</el-dropdown-item>
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
+        <span>Tom</span>
+      </div>
+    </el-header>
 
+    <!-- 主体部分，包含侧边栏和主内容 -->
     <el-container>
-      <el-header style="text-align: right; font-size: 12px">
-        <div class="toolbar">
-          <el-dropdown>
-            <el-icon style="margin-right: 8px; margin-top: 1px">
-              <setting />
-            </el-icon>
-            <template #dropdown>
-              <el-dropdown-menu>
-                <el-dropdown-item>View</el-dropdown-item>
-                <el-dropdown-item>Add</el-dropdown-item>
-                <el-dropdown-item>Delete</el-dropdown-item>
-              </el-dropdown-menu>
-            </template>
-          </el-dropdown>
-          <span>Tom</span>
-        </div>
-      </el-header>
+      <!-- 侧边栏 -->
+      <el-aside width="200px" class="aside">
+        <el-scrollbar>
+          <el-menu :default-openeds="['1', '3']">
+            <el-sub-menu index="1">
+              <template #title>
+                <el-icon><Message /></el-icon>Navigator One
+              </template>
+              <el-menu-item-group>
+                <template #title>Group 1</template>
+                <el-menu-item index="1-1">Option 1</el-menu-item>
+                <el-menu-item index="1-2">Option 2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="Group 2">
+                <el-menu-item index="1-3">Option 3</el-menu-item>
+              </el-menu-item-group>
+              <el-sub-menu index="1-4">
+                <template #title>Option4</template>
+                <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
+              </el-sub-menu>
+            </el-sub-menu>
+          </el-menu>
+        </el-scrollbar>
+      </el-aside>
 
-      <el-main>
+      <!-- 主内容区 -->
+      <el-main class="main-content">
         <el-scrollbar>
           <el-table :data="tableData">
             <el-table-column prop="date" label="Date" width="140" />
@@ -103,26 +74,45 @@ const tableData = ref(Array.from({ length: 20 }).fill(item))
 </script>
 
 <style scoped>
+.layout-container-demo {
+  height: 100vh; /* 使用视口高度，确保全屏 */
+}
+
+/* Header 样式 */
 .layout-container-demo .el-header {
-  position: relative;
   background-color: var(--el-color-primary-light-7);
   color: var(--el-text-color-primary);
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
 }
+
+/* 侧边栏样式 */
 .layout-container-demo .el-aside {
-  color: var(--el-text-color-primary);
   background: var(--el-color-primary-light-8);
+  color: var(--el-text-color-primary);
 }
+
+/* 菜单样式 */
 .layout-container-demo .el-menu {
   border-right: none;
 }
+
+/* 主内容区样式 */
 .layout-container-demo .el-main {
-  padding: 0;
+  padding: 20px;
+  background-color: #f0f2f5;
 }
-.layout-container-demo .toolbar {
-  display: inline-flex;
+
+/* 工具栏样式 */
+.toolbar {
+  display: flex;
   align-items: center;
-  justify-content: center;
-  height: 100%;
-  right: 20px;
+}
+
+/* 下拉图标样式 */
+.dropdown-icon {
+  margin-right: 8px;
+  margin-top: 1px;
 }
 </style>
